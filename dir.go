3dec178a400675b8da2a6098bf966278c5b29efd
Sebastiaan <shj.vandermeulen@gmail.com>
2021-12-29 15:31:25 +0100

    x

--- dir.go
+++ dir.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 // TODO: remove when done
@@ -13,9 +14,17 @@ var (
 	_ = log.Printf
 )
 
-// func to read identify the tree inside a directory
+// newDir takes a folder path, creates a new directory and returns an error if
+// it cannot create the new file and does not already exist.
+func newDir(dir string) error {
+	err := os.Mkdir(dir, 0755)
+	if err != nil && !os.IsExist(err) {
+		return err
+	}
+	return nil
+}
 
-// isFile takes a file path and returns true if it is a file.
+// isFile takes a file path and returns true if it represents a file.
 func isFile(fname string) bool {
 	info, err := os.Stat(fname)
 	if os.IsNotExist(err) {
@@ -28,7 +37,7 @@ func isFile(fname string) bool {
 	return true
 }
 
-// isDir takes a directory path and returns true if it is directory.
+// isDir takes a directory path and returns true if it represents a directory.
 func isDir(fname string) bool {
 	info, _ := os.Stat(fname)
 	if info.IsDir() {
@@ -37,12 +46,57 @@ func isDir(fname string) bool {
 	return false
 }
 
-// newDir creates a new directory. E.g. "c:\Directory\Folder" will create "Folder" in "Directory".
-// If "Folder" already exists, it will NOT return an error. However, if "Directory" does not exist, it will return an error.
-func newDir(dir string) error {
-	err := os.Mkdir(dir, 0755)
-	if err != nil && !os.IsExist(err) {
-		return err
+// getFnames retrieves the subdirectories and files within the source folder and stores it in Subdirs and Files.
+func getFnames(dir string) (fnames []string, err error) {
+	// Open directory
+	file, err1 := os.Open(dir)
+	if err1 != nil {
+		err = fmt.Errorf("Unable to open '%v': %v", dir, err1)
+		return
 	}
-	return nil
+	defer file.Close()
+
+	// Read all files and directories
+	list, _ := file.Readdirnames(0)
+	for _, item := range list {
+		if itemPath := dir + "\\" + item; isFile(itemPath) {
+			// File
+			fnames = append(fnames, itemPath)
+		} else {
+			// Directory
+			subFnames, err1 := getFnames(itemPath)
+			if err != nil {
+				err = fmt.Errorf("Unable to getFnames from '%v': %v", dir, err1)
+				return
+			}
+			fnames = append(fnames, subFnames...)
+		}
+	}
+	return
+}
+
+// lastSegment takes a path and returns the last segment of that path.
+// E.g. "c:\test" will return "test".
+func lastSegment(p string) string {
+	v := strings.Split(p, "\\")
+	if n := v[len(v)-1]; n != "" {
+		return n
+	}
+	return v[len(v)-2]
+}
+
+// relPath takes two paths and returns the path without the root.
+// If root does not exist in path, it returns an error.
+func relPath(root, path string) (string, error) {
+	// Check and add backslash to root for comparison to path
+	if string(root[len(root)-1]) != "\\" {
+		root += "\\"
+	}
+	// Check that path is in root
+	if root != path[:len(root)] {
+		return "", fmt.Errorf("Path not in root.\nRoot:\t%v\nPath:\t%v\n", root, path)
+	}
+	//	Remove root from path
+	path = path[len(root):]
+	return path, nil
 }
--- dir_test.go
+++ dir_test.go
@@ -3,8 +3,9 @@ package imagine
 import (
 	"fmt"
 	"log"
-	// "os"
-	// "testing"
+
+	//	"os"
+	"testing"
 )
 
 // TODO: remove when done
@@ -13,22 +14,75 @@ var (
 	_ = log.Printf
 )
 
-// func TestIsDir(t *testing.T) {
-// 	dir := ".\\temp_folder"
+// func TestDir(t *testing.T) {
+// 	dir := ".\\test_folder"
 // 	fname := "temp.txt"
-
-// 	// Create file for testing
-// 	file, err := os.Create(dir)
-// 	file.Close()
+// 	// Create dir for testing
+// 	err := newDir(dir)
 // 	if err != nil {
-// 		t.Errorf("Error creating directory '%v':\n%v", dir, err)
+// 		t.Errorf("Error creating new folder: '%v'\n%v", dir, err)
 // 	}
-// 	// Create file for testing
-// 	file, err = os.Create(dir + "\\" + fname)
+
+// 	// Test if folder is a folder
+// 	if b := isDir(dir); b != true {
+// 		t.Errorf("Error check isDir: Want:'%v' got: '%v'", true, b)
+// 	}
+
+// 	// Create folder for testing
+// 	file, err := os.Create(dir + "\\" + fname)
 // 	file.Close()
 // 	if err != nil {
 // 		t.Errorf("Error creating file '%v':\n%v", fname, err)
 // 	}
-// 	fmt.Println(isDir(dir))
-// 	fmt.Println(isDir(dir + "\\" + fname))
+
+// 	// Test if file is a file
+// 	if b := isFile(dir + "\\" + fname); b != true {
+// 		t.Errorf("Error check isFile: want '%v' got '%v'", true, b)
+// 	}
+
+// 	//
+// 	output, err := getFnames(dir)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	if output[0] != dir+"\\"+fname {
+// 		t.Errorf("Want: %v. Got: %v", dir+"\\"+fname, output[0])
+// 	}
+
+// 	// Remove file + directory
+// 	os.Remove(dir + "\\" + fname)
+// 	os.Remove(dir)
 // }
+
+func TestLastSegment(t *testing.T) {
+	cases := []struct{ input, want string }{
+		{"c:\\test", "test"},
+		{"c:\\test ", "test "},
+		{"c:\\test1\\test2\\test3", "test3"},
+	}
+	for _, c := range cases {
+		got := lastSegment(c.input)
+		if c.want != got {
+			t.Errorf("Want: '%v'. Got: '%v'.", c.want, got)
+		}
+	}
+}
+
+func TestRelPath(t *testing.T) {
+	cases := []struct{ root, path, want string }{
+		{"c:\\test1\\test2", "c:\\test1\\test2\\test3", "test3"},
+		{"c:\\test1\\test2\\", "c:\\test1\\test2\\test3", "test3"},
+		{"c:\\test1", "c:\\test1\\test2\\test3", "test2\\test3"},
+		{"c:\\test1\\test2\\test3", "c:\\test1\\test2\\test3\\test4", "test4"},
+		{"c:\\test1", "c:\\test1\\test2\\test3\\test4", "test2\\test3\\test4"},
+	}
+	for _, c := range cases {
+		got, err := relPath(c.root, c.path)
+		if err != nil {
+			t.Error(err)
+		}
+		if c.want != got {
+			t.Errorf("Want: '%v' Got: '%v'", c.want, got)
+		}
+	}
+}
